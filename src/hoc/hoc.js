3293c1bd81ec753b059f8cd31f86d28fca74d49f
silvestrevivo <silvestrevivomillan@gmail.com>
2018-08-27 22:10:17 +0200

    project set up with parcel starter

---
+++ src/hoc/hoc.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+// First version stateles Hoc Component
+// const Hoc = (WrappedComponent, className) => {
+//   return (props) => (
+//     <div className={className}>
+//       <WrappedComponent {...props} />
+//     </div>
+//   )
+// }
+
+// Second version statefull Hoc Component
+const Hoc = (WrappedComponent, className) =>
+  class extends Component {
+    render() {
+      return (
+        <div className={className}>
+          <WrappedComponent {...this.props} />
+        </div>
+      );
+    }
+  };
+
+export default Hoc;
